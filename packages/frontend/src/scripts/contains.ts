8022bc3d8947178c35b9a1945fa381780a0e61ad
ZerglingGo <zerglinggo@zerglinggo.net>
2024-01-03 18:47:30 +0900

    fix(frontend): `scripts/contains` handles null child

--- packages/frontend/src/scripts/contains.ts
+++ packages/frontend/src/scripts/contains.ts
@@ -5,9 +5,11 @@
 
 export default (parent, child, checkSame = true) => {
 	if (checkSame && parent === child) return true;
+	if (child === null) return false;
 	let node = child.parentNode;
 	while (node) {
 		if (node === parent) return true;
+		if (node === null) return false;
 		node = node.parentNode;
 	}
 	return false;
